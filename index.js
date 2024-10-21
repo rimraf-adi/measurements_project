09a5073f8af799719ade6b789cea30e9580a15cf
Aditya Kinjawadekar <kinjawadekaradi112@gmail.com>
2024-10-21 22:30:50 +0530

    feat: add file handling

--- index.js
+++ index.js
@@ -4,10 +4,35 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
+const PORT = 3000;
+const filePath = path_1.default.join(__dirname, 'sensorData.json');
 app.use(express_1.default.json());
-app.get('/', (req, res) => { res.send('server running'); });
+app.get('/', (req, res) => {
+    res.send('Server running');
+});
 app.post('/api/v1/sensor1', (req, res) => {
     const data = req.body;
-    res.send(data);
+    fs_1.default.writeFile(filePath, JSON.stringify(data, null, 2), 'utf8', (err) => {
+        if (err) {
+            return res.status(500).json({ message: 'Error writing data to file', error: err });
+        }
+        res.status(201).json({ message: 'Data successfully saved', data });
+    });
+});
+app.get('/api/v1/sensor1', (req, res) => {
+    fs_1.default.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).json({ message: 'Error reading data from file', error: err });
+        }
+        if (!data) {
+            return res.status(404).json({ message: 'No data found' });
+        }
+        res.status(200).json(JSON.parse(data));
+    });
+});
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
--- index.ts
+++ index.ts
@@ -1,11 +1,42 @@
-import express, {Request,Response} from 'express'
+import express, { Request, Response } from 'express';
+import fs from 'fs';
+import path from 'path';
 
 const app = express();
-app.use(express.json())
-app.get('/',(req: Request, res : Response)=>{res.send('server running')})
+const PORT = 3000;
+const filePath = path.join(__dirname, 'sensorData.json');
 
-app.post('/api/v1/sensor1', (req : Request, res : Response)=>{
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Server running');
+});
+
+app.post('/api/v1/sensor1', (req: Request, res: Response) => {
     const data = req.body;
-    res.send(data)
 
-})
+    fs.writeFile(filePath, JSON.stringify(data, null, 2), 'utf8', (err) => {
+        if (err) {
+            return res.status(500).json({ message: 'Error writing data to file', error: err });
+        }
+        res.status(201).json({ message: 'Data successfully saved', data });
+    });
+});
+
+app.get('/api/v1/sensor1', (req: Request, res: Response) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).json({ message: 'Error reading data from file', error: err });
+        }
+
+        if (!data) {
+            return res.status(404).json({ message: 'No data found' });
+        }
+
+        res.status(200).json(JSON.parse(data));
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
